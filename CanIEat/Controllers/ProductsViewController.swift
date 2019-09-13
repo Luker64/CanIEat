6ca1560c1d8928513819c69786dc229c88af139a
Student <student@student20.local>
2019-09-13 16:23:51 -0300

    Product Controller

--- CanIEat/Controllers/ProductsViewController.swift
+++ CanIEat/Controllers/ProductsViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+var products = [Product]()
 
+class ProductsViewController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        products = ProductDAO.getList()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
+        
+        
+        if let productCell = cell as? ProductsTableViewCell{
+            
+            let product = products[indexPath.row]
+            
+            
+        }
+        return cell
+    }
+}
 
+extension UITableViewDelegate{
+    
 }
--- CanIEat/Models/Product.swift
+++ CanIEat/Models/Product.swift
@@ -21,8 +21,9 @@ class Product {
     let brand : String
     let restrictions : [Restriction]
     let imageName : String
+    let barCode : String
     
-    init(name : String, ingredients : String, portion : String, energeticValue : Double, carbohidrates : Double, proteins : Double, totalFat : Double, fiber : Double, sodium : Double, brand: String, restrictions : [Restriction], imageName : String) {
+    init(name : String, ingredients : String, portion : String, energeticValue : Double, carbohidrates : Double, proteins : Double, totalFat : Double, fiber : Double, sodium : Double, brand: String, restrictions : [Restriction], imageName : String, barCode: String) {
         self.name = name
         self.ingredients = ingredients
         self.portion = portion
@@ -35,15 +36,16 @@ class Product {
         self.brand = brand
         self.restrictions = restrictions
         self.imageName = imageName
+        self.barCode = barCode
     }
 }
 
 class ProductDAO {
-    func getList() -> [Product]{
+    static func getList() -> [Product]{
         return [
-            Product(name: "Feijāo Cores", ingredients: "Feijāo", portion: "60g", energeticValue: 207, carbohidrates: 37, proteins: 13, totalFat: 0.9, fiber: 9.1, sodium: 0.009, brand: "Feijao Sao Joao", restrictions: [], imageName: ""),
-            Product(name: "Molho de Tomate", ingredients: "Tomate, açúcar, cebola, amido modificado, sal, extrato de levedura, salsa, manjericão, orégano, tomilho, conservador sorbato de potássio, realçador de sabor glutamato monossódico e aromatizantes. CONTÉM GLÚTEN. ALÉRGICOS: CONTÉM DERIVADOS DE CEVADA. PODE CONTER DERIVADOS DE LEITE DE VACA, SOJA E TRIGO.", portion: "60g", energeticValue: 27, carbohidrates: 5.8, proteins: 0.5, totalFat: 0, fiber: 0.6, sodium: 0.316, brand: "Quero", restrictions: [Restriction(name: "Lactose"), Restriction(name: "Soja"), Restriction(name: "Trigo")], imageName: ""),
-            Product(name: "Leite", ingredients: "Leite integral e estabilizantes trifosfato de sódio, citrato de sódio, monofosfato de sódio e difosfato de sódio.", portion: "200ml", energeticValue: 115, carbohidrates: 9.1, proteins: 6.1, totalFat: 6, fiber: 0, sodium: 0.122, brand: "Mimosa", restrictions: [Restriction(name: "Lactose")], imageName: "")
+            Product(name: "Feijāo Cores", ingredients: "Feijāo", portion: "60g", energeticValue: 207, carbohidrates: 37, proteins: 13, totalFat: 0.9, fiber: 9.1, sodium: 0.009, brand: "Feijao Sao Joao", restrictions: [], imageName: "", barCode: ""),
+            Product(name: "Molho de Tomate", ingredients: "Tomate, açúcar, cebola, amido modificado, sal, extrato de levedura, salsa, manjericão, orégano, tomilho, conservador sorbato de potássio, realçador de sabor glutamato monossódico e aromatizantes. CONTÉM GLÚTEN. ALÉRGICOS: CONTÉM DERIVADOS DE CEVADA. PODE CONTER DERIVADOS DE LEITE DE VACA, SOJA E TRIGO.", portion: "60g", energeticValue: 27, carbohidrates: 5.8, proteins: 0.5, totalFat: 0, fiber: 0.6, sodium: 0.316, brand: "Quero", restrictions: [Restriction(name: "Lactose"), Restriction(name: "Soja"), Restriction(name: "Trigo")], imageName: "", barCode: ""),
+            Product(name: "Leite", ingredients: "Leite integral e estabilizantes trifosfato de sódio, citrato de sódio, monofosfato de sódio e difosfato de sódio.", portion: "200ml", energeticValue: 115, carbohidrates: 9.1, proteins: 6.1, totalFat: 6, fiber: 0, sodium: 0.122, brand: "Mimosa", restrictions: [Restriction(name: "Lactose")], imageName: "", barCode: "")
         ]
     }
 }
