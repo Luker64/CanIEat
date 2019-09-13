437ac7e798821d91995e1530b5913c65e4f26ca7
gabrielcustodio21 <gkustodor@gmail.com>
2019-09-13 16:22:55 -0300

    storyboarddenovo

---
+++ CanIEat/Controllers/ProductsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsTableViewCell.swift
+//  CanIEat
+//
+//  Created by Student on 13/09/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
