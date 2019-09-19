673692a684c6bacbbb0fde9368feedef6e441d4b
CanIEat <canieat config --local --canieatit2019@gmail.com>
2019-09-19 16:05:01 -0300

    newVersion

--- CanIEat/Controllers/ChatController.swift
+++ CanIEat/Controllers/ChatController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 import MessageKit
+import InputBarAccessoryView
+
+
+
 
 class ChatController: MessagesViewController {
+    
+    @IBAction func ChatAction(_ sender: Any) {
+        performSegue(withIdentifier: "showNext", sender: nil)
+    }
     
     var chatService: ChatService!
     var messages: [Message] = []
@@ -83,14 +91,34 @@ extension ChatController: MessagesDisplayDelegate {
 }
 
 extension ChatController: MessageInputBarDelegate {
-    func messageInputBar(
-        _ inputBar: MessageInputBar,
-        didPressSendButtonWith text: String) {
-        
+    
+    internal func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
         chatService.sendMessage(text)
-        if text.lowercased() == "hello"{
-            chatService.sendMessage("ola")
+        switch text.lowercased() {
+        case "ola":
+            chatService.sendMessage("Veggie responde: Olá, eu sou sua assistente do Can I Eat? Meu nome é Veggie, qual o seu nome?")
+        case "gabriel":
+            chatService.sendMessage("Veggie responde: O aplicativo foi desenvolvido para auxiliar você a escolher o melhor produto de acordo com suas restrições alimentares ou dietas recomendadas. Me conte, Gabriel, qual sua finalidade com o app? Restricao alimentar ou voce esta seguindo alguma dieta?")
+        case "restricao alimentar":
+            chatService.sendMessage("Veggie responde: Entendi. Me informe os alimentos ou produtos que você não pode consumir (ex: lactose, cafeína, glúten etc)")
+        case "lactose":
+            chatService.sendMessage("Veggie responde: Ok, Gabriel. Alguma restrição a mais?")
+        case "nao":
+            chatService.sendMessage("Veggie responde: Pronto. Você já pode utilizar o app para pesquisas dentro da nossa biblioteca por alimentos compatíveis com as suas restrições. Caso nāo encontre o que deseja, nosso aplicativo possibilita você escanear o código de barras de um produto ou QR Code para saber se os ingredientes sāo adequados para suas necessidades.")
+        case "dieta":
+            chatService.sendMessage("Veggie responde: Qual tipo de dieta voce pretende seguir?")
+        case "vegana":
+            chatService.sendMessage("Veggie responde: Entendido, me informe suas preferências alimentares para eu indicar alimentos saudáveis de acordo com o seu gosto. (ex: verduras, frutas, grãos etc)")
+        case "verdura":
+            chatService.sendMessage("Veggie responde: E tem alguma coisa que você não gosta?")
+        case "pvt":
+            chatService.sendMessage("Veggie responde: Ok, nao te recomendarei pvt, entao!")
+            chatService.sendMessage("Veggie responde: Ok, Gabriel. Alguma outra informacao?")
+        default:
+            chatService.sendMessage("Veggie responde: Não entendi sua resposta. Você pode escrever novamente?")
+            
         }
+            
         inputBar.inputTextView.text = ""
     }
 }
